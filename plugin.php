0cf5627c2234377d6079e44392b7ef441abe9746
Fred Neumann <fred.neumann@fau.de>
2019-07-24 17:06:37 +0200

    Version 1.2.0 for ILIAS 5.4

--- plugin.php
+++ plugin.php
@@ -5,12 +5,12 @@
 $id = "tarc_cron";
 
 // code version; must be changed for all code changes
-$version = "1.1.1";
+$version = "1.2.0";
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "5.2.9";
-$ilias_max_version = "5.3.999";
+$ilias_min_version = "5.4.4";
+$ilias_max_version = "5.4.999";
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Fred Neumann";
